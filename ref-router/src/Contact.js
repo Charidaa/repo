e4f7083b94c77a0e6b97770c05bfdf61c18b37df
charidaa <6129010005@htc.ac.th>
2023-07-18 12:15:02 +0700

    ref-router

---
+++ ref-router/src/Contact.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Layout } from "./App";
+
+function contact() {
+    return (
+    <>
+    <Layout />
+    <h3>ubon ratchathani university</h3>
+    <div>
+    Ubon Ratchathani University
+    <br />
+    85 Sathonlamark Road
+    <br /> 
+    Warin Chamrap District
+    <br />
+    Ubon Ratchathani
+    <br />
+    Thailand 34190
+    </div>
+    </>
+    );
+}
+
+export default contact;
